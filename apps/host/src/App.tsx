78360bc7d199977711a1d84b7f1f17e15c07b333
Subhas Sing <Subhas.Sing@verramobility.com>
2026-01-12 12:42:17 -0700

    all are working wraping

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 
-import { createStore, Provider, atom, useAtom } from 'jotai';
+import { getDefaultStore, Provider, atom, useAtom } from 'jotai';
 import ErrorBoundary from './ErrorBoundary';
 // import { useAtomsDebugValue } from 'jotai-devtools';
 import { useAtomsDevtools } from 'jotai-devtools/utils';
@@ -11,34 +11,27 @@ const Footer = React.lazy(() => import('headerApp/Footer'));
 
 //const RemoteStore = React.lazy(() => import('contentApp/Store'));
 
-const customStore = createStore();
-// const DebugAtoms = () => {
-//   useAtomsDebugValue()
-//   return null;
-// }
 
-// const textAtom = atom('hello')
-// textAtom.debugLabel = 'textAtom'
-
-// const lenAtom = atom((get) => get(textAtom).length)
-// lenAtom.debugLabel = 'lenAtom'
 
 const AtomsDevtools: React.FC<{ children: any }> = ({ children }) => {
   useAtomsDevtools('Shell App')
   return children;
 }
 
-// const TextBox = () => {
-//   const [text, setText] = useAtom(textAtom)
-//   const [len] = useAtom(lenAtom)
-//   return (
-//     <span>
-//       <input value={text} onChange={(e) => setText(e.target.value)} />({len})
-//     </span>
-//   )
-// }
+const customStore = getDefaultStore();
 const counter = atom(0);
 counter.debugLabel = 'counterAtom-host';
+
+const Outer = () => {    
+    return (
+        <Provider store={customStore}>
+            <AtomsDevtools>
+                <App />
+            </AtomsDevtools>
+        </Provider>
+    );
+};
+
 const App = () => {
   const [count, setCounter] = useAtom(counter);
   const onClick = () => setCounter(prev => prev + 1);
@@ -46,13 +39,9 @@ const App = () => {
     <>
       <ErrorBoundary>
           <div className='flex flex-col items-center justify-center max-h-screen border-2 bg-gray-500'>
-            Test
-            <Provider store={customStore}>
-              <AtomsDevtools>
+            Test            
               <h1>{count}</h1>
-              <button onClick={onClick}>Click</button>
-              </AtomsDevtools>
-            </Provider>
+              <button onClick={onClick}>Click</button>            
             <div className='w-full'>
             <Suspense fallback="Loading header...">
               <Header />
@@ -77,4 +66,4 @@ const App = () => {
 //   return <h1>Hello Webpack + React!</h1>;
 // };
 
-export default App;
+export default Outer;
