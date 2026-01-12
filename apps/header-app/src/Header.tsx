538da7b0caebca799724e444fee3de2224d9e17f
Subhas Sing <Subhas.Sing@verramobility.com>
2026-01-12 12:06:17 -0700

    seems to be working

--- apps/header-app/src/Header.tsx
+++ apps/header-app/src/Header.tsx
@@ -3,29 +3,31 @@ import React from 'react';
 
 import { createStore, Provider, atom, useAtom } from 'jotai';
 import { useAtomsDevtools } from 'jotai-devtools';
-// const AtomsDevtools: React.FC<{ children: any }> = ({ children }) => {
-//     useAtomsDevtools('Header App')
-//     return children
-// }
-// const customStore = createStore();
-// const counter = atom(0);
-// const Outer = () => {    
-//     return (
-//         <Provider store={customStore}>
-//             <AtomsDevtools>
-//                 <Header />
-//             </AtomsDevtools>
-//         </Provider>
-//     );
-// };
+const AtomsDevtools: React.FC<{ children: any }> = ({ children }) => {
+    useAtomsDevtools('Header App')
+    return children
+}
+const customStore = createStore();
+const counter = atom(0);
+const Outer = () => {    
+    return (
+        <Provider store={customStore}>
+            <AtomsDevtools>
+                <Header />
+            </AtomsDevtools>
+        </Provider>
+    );
+};
 const Header = () => {
-    //const [count, setCounter] = useAtom(counter);
-    //const onClick = () => setCounter(prev => prev + 1);
+    const [count, setCounter] = useAtom(counter);
+    const onClick = () => setCounter(prev => prev + 1);
     //console.log(`Current Version: ${VERSION}, Branch: ${BRANCH}`);
     const BRANCH = COMMITHASH || 'unknown';
     return (
         <header style={{ background: '#f0f0f0', padding: '20px' }} className='bg-sky-500'>
             <h1>Micro Frontend Header {BRANCH}</h1>
+            <h1>{count}</h1>
+            <button onClick={onClick}>Click</button>
             <nav>
                 <a href="#" style={{ marginRight: '10px' }}>Home</a>
                 <a href="#">About</a>
@@ -34,4 +36,4 @@ const Header = () => {
     );
 };
 
-export default Header;
+export default Outer;
